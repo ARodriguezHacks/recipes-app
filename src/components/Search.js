6ac0a72e028eafbebf7bf181bf5feb751588e498
Angie Rodriguez <angrodri902@gmail.com>
2021-02-10 00:47:08 -0800

    Initial commit and create Search component

---
+++ src/components/Search.js
@@ -0,0 +1,11 @@
+function Search() {
+  return (
+    <form>
+      <label htmlFor="search">Search</label>
+      <br />
+      <input type="search" name="search" id="search" />
+    </form>
+  );
+}
+
+export default Search;
